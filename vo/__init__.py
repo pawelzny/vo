2a88f9e9a2d814760d6e5a768f7f5140a006ac91
Paweł Zadrożny <pawel.zny@gmail.com>
2018-03-14 16:00:11 +0100

    Bump version: 0.1.0 → 0.2.0

--- vo/__init__.py
+++ vo/__init__.py
@@ -4,5 +4,5 @@ from vo.Value import Value, ValueModificationForbidden
 
 __author__ = 'Paweł Zadrożny'
 __copyright__ = 'Copyright (c) 2017, Pawelzny'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __all__ = ['Value', 'ValueModificationForbidden']
