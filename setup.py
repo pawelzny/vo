8c0ec83ba4c5ee4f321b59581afb8a680ffb1b96
Paweł Zadrożny <pawel.zny@gmail.com>
2018-03-14 15:05:09 +0100

    requirements.txt moved to PipFile, and remove bin dir

--- setup.py
+++ setup.py
@@ -1,24 +1,80 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+import pathlib
+import re
+import subprocess
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+__author__ = 'Paweł Zadrożny'
+__copyright__ = 'Copyright (c) 2018, Pawelzny'
+__requires__ = ['pipenv']
+
+base_dir = pathlib.Path(__file__).parent
+
+with open('README.rst', 'r') as readme_file:
+    readme = readme_file.read()
+
+with open('LICENSE', 'r') as license_file:
+    # noinspection PyShadowingBuiltins
+    license = license_file.read()
 
 
-with open('README.rst') as f:
-    readme = f.read()
+def get_version(*file_paths):
+    """Retrieves the version from project/__init__.py"""
+    filename = os.path.join(os.path.dirname(__file__), *file_paths)
+    version_file = open(filename).read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
 
-with open('LICENSE') as f:
-    license = f.read()
 
-setup(name='vo',
-      version='0.1.0',
-      description='DDD Value Objects implementation',
-      long_description=readme,
-      keywords='value data object DDD',
-      author='Pawel Zadrozny @pawelzny',
-      author_email='pawel.zny@gmail.com',
-      url='https://github.com/pawelzny/vo',
-      license=license,
-      test_suite='nose.collector',
-      tests_require=['nose', 'coverage'],
-      packages=find_packages(exclude=('tests', 'docs', 'bin')),
-      zip_safe=False)
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+
+    def run(self):
+        subprocess.check_call(['pipenv', 'install', '--dev', '--deploy', '--system'])
+        develop.run(self)
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+
+    def run(self):
+        subprocess.check_call(['pipenv', 'install', '--deploy', '--system'])
+        install.run(self)
+
+
+setup(
+    name='vo',
+    version=get_version('vo', '__init__.py'),
+    description='DDD Value Objects implementation',
+    long_description=readme,
+    keywords='value data object DDD',
+    author='Paweł Zadrożny @pawelzny',
+    author_email='pawel.zny@gmail.com',
+    url='https://github.com/pawelzny/vo',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs', 'bin')),
+    package_dir={'vo': 'vo'},
+    include_package_data=True,
+    use_scm_version=True,
+    install_requires=['setuptools_scm'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: ISC License (ISCL)',
+    ],
+    cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+    },
+)
