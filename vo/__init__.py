0136425a39aa7eca5b0b17cce3b14e702ee5dab8
Paweł Zadrożny <pawel.zny@gmail.com>
2018-03-19 11:54:09 +0100

    Bump version: 0.3.0 → 0.3.1

--- vo/__init__.py
+++ vo/__init__.py
@@ -4,5 +4,5 @@ from vo.value import Value, ImmutableInstanceError
 
 __author__ = 'Paweł Zadrożny'
 __copyright__ = 'Copyright (c) 2017, Pawelzny'
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 __all__ = ['Value', 'ImmutableInstanceError']
